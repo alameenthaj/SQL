c0134884aa605bdcba67a16e7b118584ea687967
alameenthaj <alluthaj0000@gmail.com>
2026-09-22 11:37:23 +0530

    Add DAY5_hw.sql for book management operations including table creation, data insertion, and queries

---
+++ DAY5_hw.sql
@@ -0,0 +1,13 @@
+CREATE TABLE books(id int,title varchar(20),author varchar(20),price decimal(10,2),stock int);
+
+INSERT INTO books VALUES(1, 'The Alchemist', 'Paulo Coelho', 350, 50),
+    (2, 'Atomic Habits', 'James Clear', 450, 40),
+    (3, 'The Psychology of Money', 'Morgan Housel', 400, 30),
+    (4, 'Ikigai', 'Francesc Miralles', 300, 60),
+    (5, 'Deep Work', 'Cal Newport', 500, 20);
+
+1.select * from books where price<450 and stock>30;
+2.update books set stock=stock+45,price=420 where title='Deep Work';
+3.delete from books where title="ikigai";
+4.select avg(price),count(*) from books;
+5. select price from books order by price desc limit 3;
